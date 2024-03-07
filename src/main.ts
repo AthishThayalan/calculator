4f17b167f44fb1a3d6e94feb48aede8adf86bf10
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-07 11:52:34 +0000

    Got it to add to the "operation" variable depending on the inner text of the button clicked.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,27 @@ import "./style.scss";
 
 console.log("The application is running ...");
 
-// target the display
+//Variables to use
 const display = document.querySelector<HTMLInputElement>("display");
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
 let operation = "";
+
+//Update Display
+const updateDisplay = (): void => {
+  if (!display) {
+    throw new Error("Display error");
+  }
+  display.value = operation;
+};
+//Add to operation / Display Output
+const addToOperation = (value: string): void => {
+  operation += value;
+  console.log(operation);
+  updateDisplay();
+};
+
+//iterate through each button and add the CLICK capabilities.
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => addToOperation(button.innerText));
+});
