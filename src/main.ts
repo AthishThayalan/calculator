535ed0724ec01f76ec7b03d6ce5b7cf5c6be53af
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-07 14:35:08 +0000

    Able to add to display by clicking buttons . Add added a conditional statement that only allows 1 decimal point to be entered.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const display = document.querySelector<HTMLInputElement>(
   ".calculator__display"
 );
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
-let operation = "";
+let operation: string = "";
 
 //Update Display
 const updateDisplay = (): void => {
@@ -16,10 +16,30 @@ const updateDisplay = (): void => {
   }
   display.value = operation;
 };
+//clear operation
+const clearOperation = (): void => {
+  operation = "";
+  updateDisplay();
+};
+//DEL
+const removeLastDigit = (): void => {
+  operation = operation.slice(0, -1);
+  updateDisplay();
+};
 //Add to operation / Display Output
 const addToOperation = (value: string): void => {
-  operation += value;
-  console.log(operation);
+  if (
+    (Number(value) >= 0 && Number(value) <= 9) ||
+    (value === "." && !operation.includes(".")) // Makes sure that you can only add one decimal point.
+  ) {
+    operation += value;
+    console.log(operation);
+  } else if (value === "AC") {
+    clearOperation();
+  } else if (value === "DEL") {
+    removeLastDigit();
+  }
+
   updateDisplay();
 };
 
