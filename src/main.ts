dc1dbf5117d806a0fcb9fb6e72f2706425f494d6
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-07 07:46:36 +0000

    Added some styling on operators using BEM naming convention. Having trouble appending numbers to the display.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./style.scss";
+
+// target the display
+const display = document.getElementById("display");
+
+//Add numbers to display
+const append = (value: string): void => {
+  console.log("append");
+  display.value += value;
+  console.log(`Added ${value}`);
+};
