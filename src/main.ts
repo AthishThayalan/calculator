98b6244cad108a1c2429d7999292a840defeae3e
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-11 15:57:12 +0000

    Added the ability to toggle dark mode on and off

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,31 @@ import "./style.scss";
 
 console.log("The application is running ...");
 
-//Variables to use
 const display = document.querySelector<HTMLInputElement>(
   ".calculator__display"
 );
+const calculator = document.querySelector<HTMLDivElement>(".calculator");
 const buttons = document.querySelectorAll<HTMLButtonElement>("button");
 let operation: string = "";
 
 const operators: string[] = ["+", "-", "*", "/", "%"];
 
-//Update Display
+if (!calculator || !display) {
+  throw new Error("Error.");
+}
+
 const updateDisplay = (): void => {
   if (!display) {
     throw new Error("Display error");
   }
   display.value = operation;
 };
-//clear operation
+
 const clearOperation = (): void => {
   operation = "";
   updateDisplay();
 };
-//DEL
+
 const removeLastDigit = (): void => {
   operation = operation.slice(0, -1);
   updateDisplay();
@@ -42,10 +45,10 @@ const shuntingYardAlgorithm = (operation: string): string[] => {
   console.log("STARTING OPERATION : " + operation);
   const stack = [];
   const queue = [];
-  // Regular expression to split by numbers and operators.
+
   const regex = /(\d+|[+\-*\/%])/g;
-  const tokens = operation.match(regex); // returns arr split by nums and operators.
-  //
+  const tokens = operation.match(regex);
+
   console.log("Reg expression thats been split : " + tokens);
   if (!tokens) {
     throw new Error("Invalid operation string");
@@ -92,7 +95,6 @@ const postFixStackEvaluator = (operation: string[]): string => {
   const stack: string[] = [];
   console.log("Starting post fix: " + operation);
   for (let i: number = 0; i < operation.length; i++) {
-    // at this point precedence doesn't matter . op[i] is either number or operator.
     console.log("post fix stack iteration " + i + ": " + stack);
     const token = operation[i];
     if (!operators.includes(token)) {
@@ -150,18 +152,18 @@ const calculateOperation = (operation: string): number => {
 const evaluateOperation = (): void => {
   if (
     operation &&
-    !operators.includes(operation.charAt(operation.length - 1)) // only lets you press "=" if last element is a number
+    !operators.includes(operation.charAt(operation.length - 1))
   ) {
     const result: number = calculateOperation(operation);
     operation = result.toString();
     updateDisplay();
   }
 };
-//Add to operation / Display Output
+
 const addToOperation = (value: string): void => {
   if (
     (Number(value) >= 0 && Number(value) <= 9) ||
-    (value === "." && !operation.includes(".")) // Makes sure that you can only add one decimal point.
+    (value === "." && !operation.includes("."))
   ) {
     addValue(value);
   } else if (value === "AC") {
@@ -170,7 +172,7 @@ const addToOperation = (value: string): void => {
     removeLastDigit();
   } else if (
     operators.includes(value) &&
-    !operators.includes(operation.slice(-1)) // this checks that you can only enter an operator , if it is preceeded by a number
+    !operators.includes(operation.slice(-1))
   ) {
     addValue(value);
   } else {
@@ -179,8 +181,34 @@ const addToOperation = (value: string): void => {
   updateDisplay();
 };
 
-//iterate through each button and add the CLICK capabilities.
-
 buttons.forEach((button) => {
   button.addEventListener("click", () => addToOperation(button.innerText));
 });
+
+const darkModeSwitch =
+  document.querySelector<HTMLDivElement>(".dark-mode-switch");
+
+if (darkModeSwitch) {
+  darkModeSwitch.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const toggleHandle = document.querySelector(
+      ".toggle-handle"
+    ) as HTMLElement;
+    if (toggleHandle) {
+      toggleHandle.style.transform = this.classList.contains("active")
+        ? "translateX(20px)"
+        : "translateX(0)";
+    }
+    const isDarkModeActive = darkModeSwitch.classList.contains("active");
+    document.body.style.backgroundColor = isDarkModeActive ? "beige" : "";
+    calculator.style.backgroundColor = isDarkModeActive
+      ? `rgb(45, 42, 42)`
+      : "";
+    buttons.forEach((button) => {
+      button.style.backgroundColor = isDarkModeActive ? "rgb(55, 52, 52)" : "";
+      button.style.color = isDarkModeActive ? "beige" : "";
+    });
+    display.style.backgroundColor = isDarkModeActive ? `rgb(45, 42, 42)` : "";
+    display.style.color = isDarkModeActive ? "beige" : "";
+  });
+}
